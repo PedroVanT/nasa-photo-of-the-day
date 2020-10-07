7692a8a5c4e60c9ca4c855ed07474dc263304191
Pedro Van Thienen <pedrovanthienen@gmail.com>
2020-10-07 19:23:32 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+
+// npm install axios — this installs axios into the current project
+
+//import axios into this JS file
+// import axios from "axios";
+
+//check if install works
+// console.log(axios);
 
 function App() {
   return (
+    // <Header />
+    // <PictureGallery />
+
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        app! Have fun{" "}
+        <span role="img" aria-label="go!">
+          🚀
+        </span>
+        !
       </p>
     </div>
   );
