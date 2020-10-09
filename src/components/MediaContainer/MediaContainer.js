d3e09997949f690dd1137f56268b423b203529d1
Pedro Van Thienen <pedrovanthienen@gmail.com>
2020-10-08 22:38:31 -0400

    styled

--- src/components/MediaContainer/MediaContainer.js
+++ src/components/MediaContainer/MediaContainer.js
@@ -1,15 +1,31 @@
 import React from "react";
 
+import styled from "styled-components";
+// 105bd8
+const ExplanationStyled = styled.div`
+background-color: #061f4a;
+padding-top:5px;
+padding-bottom:5px;
+p{
+  max-width: 70%;
+  margin-left:15%;
+  font-size:1.1rem;
+  color:#fae3f7;
+}
+h2{
+  color:white;
+}
+`
 const MediaContainer = (props) => {
   const { dataObj } = props;
   const { title, url, explanation } = dataObj;
   if (!dataObj) return <h3>Loading...</h3>;
   return (
-    <div>
+    <ExplanationStyled>
       <h2>{title}</h2>
       <img src={url} alt={title} />
       <p>{explanation}</p>
-    </div>
+    </ExplanationStyled>
   );
 };
 
--- src/components/MediaTitle/MediaTitle.js
+++ src/components/MediaTitle/MediaTitle.js
@@ -1,4 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+// 206be8 try this for background color of Header
+// fff600
+const Header = styled.div`
+  background-color: #dce4ef;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  h1 {
+    color: white;
+    font-size: 2.6rem;
+  }
+  h3 {
+    color: black;
+  }
+`;
 
 const MediaTitle = (props) => {
   const { dataObj } = props;
@@ -6,11 +21,11 @@ const MediaTitle = (props) => {
   console.log(dataObj.date);
   if (!dataObj.date) return <h3>Loading...</h3>;
   return (
-    <div>
+    <Header>
       <h1>Picture Of The Day!</h1>
-      <h3>Date</h3>
+      <h3>Date: </h3>
       <h3>{dataObj.date}</h3>
-    </div>
+    </Header>
   );
 };
 
